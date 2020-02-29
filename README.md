5391a917bc585d426049b20ecb04e1e105a970e4
faimi <51162351+faimi@users.noreply.github.com>
2020-02-29 21:24:36 +0800

    实现布局

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ## 项目简介
 ## npm
-- npm install antd --save          安装Ant Design
+- npm install antd --save               安装Ant Design
+
+- npm install --save redux              安装Redux
+- npm install --save react-redux        安装React绑定库
+- npm install --save-dev redux-devtools   安装开发者工具
+
+- npm install -S react-router
+- npm install --save react-router-dom   
+- npm install history --save
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router,Route} from 'react-router-dom';
+import store from './redux';
+import {Provider} from 'react-redux';
+import {createBrowserHistory} from 'history';
 import './index.css';
-import App from './App';
+import App from './pages/MainPageLayout/App';
+import 'antd/dist/antd.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const history=createBrowserHistory();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(
+    <Provider store={store}>
+        <Router history={history}>
+        <Route path="/" component={App}/>
+        </Router>
+    </Provider>,document.getElementById('root')
+);
+
+
 serviceWorker.unregister();
